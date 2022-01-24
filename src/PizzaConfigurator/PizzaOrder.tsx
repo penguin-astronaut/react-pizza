b57e13b385146cee38242c7589141fa89357d89c
Vadim Kasatkin <penguin2804@gmail.com>
2022-01-24 22:40:08 +0400

    refactor(pizzaorder.tsx): check empty ingridients

--- src/PizzaConfigurator/PizzaOrder.tsx
+++ src/PizzaConfigurator/PizzaOrder.tsx
@@ -10,9 +10,16 @@ export const PizzaOrder = ({ pizza }: IPizzaOrderProps) => (
     <h2>Ваш заказ:</h2>
     <p>Размер: {pizza.size}</p>
     <p>Тесто: {pizza.dough}</p>
-    <p>Сыр: {pizza.cheese.join(',')}</p>
-    <p>Мясо: {pizza.meat.join(',')}</p>
-    <p>Овощи: {pizza.vegetables.join(',')}</p>
+    <p>
+      Сыр: {pizza.cheese.length === 0 ? 'Без сыра' : pizza.cheese.join(',')}
+    </p>
+    <p>Мясо: {pizza.meat.length === 0 ? 'Без мяса' : pizza.meat.join(',')}</p>
+    <p>
+      Овощи:{' '}
+      {pizza.vegetables.length === 0
+        ? 'Без овощей'
+        : pizza.vegetables.join(',')}
+    </p>
     <button>Оплатить</button>
   </div>
 );
