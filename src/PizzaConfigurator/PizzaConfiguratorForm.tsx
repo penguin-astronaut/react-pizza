213f55a0f504bb546b4ece8f531dfdfb6a851383
Vadim Kasatkin <penguin2804@gmail.com>
2022-01-25 20:42:43 +0400

    refactor: move methods to jsx

--- src/PizzaConfigurator/PizzaConfiguratorForm.tsx
+++ src/PizzaConfigurator/PizzaConfiguratorForm.tsx
@@ -4,15 +4,6 @@ import { PizzaConfiguratorRadioGroup } from './PizzaConfiguratorRadioGroup';
 import { PizzaConfiguratorCheckGroup } from './PizzaConfiguratorCheckGroup';
 import { IPizza } from './PizzaConfigurator';
 
-const data = {
-  size: [30, 35],
-  dough: ['пышное', 'тонкое'],
-  sauce: ['томатный', 'белый', 'острый'],
-  cheese: ['моцарелла', 'чеддер', 'дор блю'],
-  vegetables: ['помидор', 'грибы', 'перец'],
-  meat: ['бекон', 'пепперони', 'ветчина'],
-};
-
 interface IPizzaConfiguratorFormProps {
   createPizza: React.Dispatch<React.SetStateAction<IPizza>>;
 }
@@ -27,24 +18,21 @@ export const PizzaConfiguratorForm = ({
   const [vegetables, setVegetables] = useState<string[]>([]);
   const [meat, setMeat] = useState<string[]>([]);
 
+  const data = {
+    size: [30, 35],
+    dough: ['пышное', 'тонкое'],
+    sauce: ['томатный', 'белый', 'острый'],
+    cheese: ['моцарелла', 'чеддер', 'дор блю'],
+    vegetables: ['помидор', 'грибы', 'перец'],
+    meat: ['бекон', 'пепперони', 'ветчина'],
+  };
+
   const price = calculatePizzaPrice({
     meat,
     cheese,
     vegetables,
     size: pizzaSize,
   });
-
-  const updatePizzaSize = (event: ChangeEvent<HTMLInputElement>) => {
-    setPizzaSize(parseInt(event.target.value, 10));
-  };
-
-  const updateDoughType = (event: ChangeEvent<HTMLInputElement>) => {
-    setDoughType(event.target.value);
-  };
-
-  const updateSauce = (event: ChangeEvent<HTMLInputElement>) => {
-    setSauce(event.target.value);
-  };
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -68,19 +56,25 @@ export const PizzaConfiguratorForm = ({
           values={data.size}
           title={'Выберете размер'}
           checkedValue={pizzaSize}
-          onChange={updatePizzaSize}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setPizzaSize(parseInt(event.target.value, 10));
+          }}
         />
         <PizzaConfiguratorRadioGroup
           values={data.dough}
           title={'Выберете тесто'}
           checkedValue={doughType}
-          onChange={updateDoughType}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setDoughType(event.target.value);
+          }}
         />
         <PizzaConfiguratorRadioGroup
           values={data.sauce}
           title={'Выберете соус'}
           checkedValue={sauce}
-          onChange={updateSauce}
+          onChange={(event: ChangeEvent<HTMLInputElement>) => {
+            setSauce(event.target.value);
+          }}
         />
         <PizzaConfiguratorCheckGroup
           values={data.cheese}
