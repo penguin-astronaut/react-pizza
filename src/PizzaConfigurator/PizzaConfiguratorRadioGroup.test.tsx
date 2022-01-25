5cd117d5f6e6846a60709ed20d607f7ad4bcbfad
Vadim Kasatkin <penguin2804@gmail.com>
2022-01-25 23:08:38 +0400

    test: create Tests from PizzaConfiguratorRadioGroup

---
+++ src/PizzaConfigurator/PizzaConfiguratorRadioGroup.test.tsx
@@ -0,0 +1,37 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { PizzaConfiguratorRadioGroup } from './PizzaConfiguratorRadioGroup';
+
+describe('PizzaConfiguratorRadioGroup', () => {
+  it('render', () => {
+    render(
+      <PizzaConfiguratorRadioGroup
+        values={['radio_1', 'radio_2']}
+        title={'Radio group'}
+        checkedValue={'radio_1'}
+        onChange={() => {}}
+      />
+    );
+
+    expect(screen.getByText('Radio group')).toBeTruthy();
+    expect(screen.queryAllByText(/radio_\d+/).length).toBe(2);
+  });
+
+  it('onChange', () => {
+    const onChange = jest.fn();
+
+    render(
+      <PizzaConfiguratorRadioGroup
+        values={['radio_1', 'radio_2', 'radio_3', 'radio_4']}
+        title={'Radio group'}
+        checkedValue={'radio_1'}
+        onChange={onChange}
+      />
+    );
+
+    fireEvent.click(screen.getByText('radio_2'));
+    fireEvent.click(screen.getByText('radio_4'));
+    fireEvent.click(screen.getByText('radio_3'));
+
+    expect(onChange).toHaveBeenCalledTimes(3);
+  });
+});
