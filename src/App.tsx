c4e5f45b8ef605e0cafc31ac14a6a69df67e3d16
Vadim Kasatkin <penguin2804@gmail.com>
2022-02-03 22:29:36 +0400

    feat: login and register components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { PizzaConfigurator } from './PizzaConfigurator/PizzaConfigurator';
 import './App.css';
+import { Login } from './Login';
+import { Register } from './Register';
 
 function App() {
   return (
     <div className="App">
-      <PizzaConfigurator />
+      <Routes>
+        <Route path="/" element={<PizzaConfigurator />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
@@ -17,7 +18,9 @@ Sentry.init({
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
