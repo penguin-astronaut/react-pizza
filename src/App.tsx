ecb5080309491a90af5dcc54019fa20c83fce1e4
Vadim Kasatkin <penguin2804@gmail.com>
2022-02-05 13:17:29 +0400

    feat: add navbar to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
 import { PizzaConfigurator } from './PizzaConfigurator/PizzaConfigurator';
 import './App.css';
 import { Login } from './Login';
@@ -11,6 +11,26 @@ import { Receipt } from './Receipt';
 function App() {
   return (
     <div className="App">
+      <nav>
+        <NavLink className={'link'} to={'/'}>
+          Конфигуратор
+        </NavLink>
+        <NavLink className={'link'} to={'/login'}>
+          Войти
+        </NavLink>
+        <NavLink className={'link'} to={'/register'}>
+          Регистрация
+        </NavLink>
+        <NavLink className={'link'} to={'/order'}>
+          Заказ
+        </NavLink>
+        <NavLink className={'link'} to={'/orders'}>
+          История заказов
+        </NavLink>
+        <NavLink className={'link'} to={'/receipt'}>
+          Чек
+        </NavLink>
+      </nav>
       <Routes>
         <Route path="/" element={<PizzaConfigurator />} />
         <Route path="/login" element={<Login />} />
